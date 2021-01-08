8bf2756a8a45a6f71b8c291247bf459530db5c51
Florian Beaufumé <fbeaufume@gmail.com>
2021-01-08 17:55:55 +0100

    Added Tailwind configuration

---
+++ mockups/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}

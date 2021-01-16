97029ccb8f159587770c1f99ebfc0e1370d2e000
Florian Beaufumé <fbeaufume@gmail.com>
2021-01-16 16:56:10 +0100

    Added enemies domain model

--- src/app/fight/fight.service.ts
+++ src/app/fight/fight.service.ts
@@ -1,12 +1,10 @@
 import {Injectable} from '@angular/core';
-import {Character, Party} from './model';
+import {Character, Enemy, Group, Party} from './model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FightService {
-
-  test = 'hello';
 
   party: Party = new Party([
       new Character('Melkan', 'Warrior', 1, 20, false, 50),
@@ -14,30 +12,14 @@ export class FightService {
       new Character('Arwin', 'Paladin', 1, 20, true, 50)],
     [
       new Character('Faren', 'Archer', 1, 20, false, 50),
-      new Character('Harika', 'Sorceress', 1, 20, true, 50),
+      new Character('Harika', 'Mage', 1, 20, true, 50),
       new Character('Nairo', 'Priest', 1, 20, true, 50)
     ]);
 
-  // TODO FBE find a way to use this syntax without the redundant attributes (life, lifePercent, bonuses, etc)
-  party2: Party = {
-    row1Characters: [
-      {
-        name: 'Melkan',
-        clazz: '',
-        level: 1,
-        life: 20,
-        lifeMax: 20,
-        lifePercent: 1,
-        useMana: false,
-        energy: 50,
-        energyMax: 50,
-        energyPercent: 1,
-        bonuses: [],
-        maluses: []
-      }
-    ],
-    row2Characters: []
-  };
+  group: Group = new Group([], [], [
+    new Enemy("Bear A", 28),
+    new Enemy("Bear B", 28)
+  ])
 
   constructor() {
   }
--- src/app/fight/model.ts
+++ src/app/fight/model.ts
@@ -1,5 +1,74 @@
 // The model classes of the application
 
+// Base class for enemies and characters
+export class Creature {
+
+  life: number;
+
+  lifePercent: number;
+
+  // Bonuses, a.k.a. "buffs"
+  bonuses: string[] = [];
+
+  // Maluses, a.k.a. "debuffs"
+  maluses: string[] = [];
+
+  constructor(
+    public name: string,
+    public lifeMax: number
+  ) {
+    this.life = lifeMax;
+    this.lifePercent = 100 * this.life / lifeMax;
+  }
+}
+
+// An enemy
+export class Enemy extends Creature {
+
+  constructor(name: string, lifeMax: number) {
+    super(name, lifeMax);
+  }
+}
+
+// A group of enemies
+export class Group {
+
+  constructor(
+    // First row enemies
+    public row1Enemies: Enemy[],
+    // Second row enemies
+    public row2Enemies: Enemy[],
+    // Third row enemies
+    public row3Characters: Enemy[]) {
+  }
+}
+
+// A party character
+export class Character extends Creature {
+
+  // Current mana or tech points (depends on the character class)
+  energy: number;
+
+  energyPercent: number;
+
+  constructor(
+    name: string,
+    // Character class, could be an enum
+    public clazz: string,
+    public level: number,
+    lifeMax: number,
+    // True for mana based character class, false for tech based
+    public useMana: boolean,
+    // Max mana or tech points (depends on the character class)
+    public energyMax: number
+  ) {
+    super(name, lifeMax);
+
+    this.energy = energyMax;
+    this.energyPercent = 100 * this.energy / energyMax;
+  }
+}
+
 // The player party
 export class Party {
 
@@ -8,40 +77,5 @@ export class Party {
     public row1Characters: Character[],
     // Back row characters
     public row2Characters: Character[]) {
-  }
-}
-
-// A party character
-export class Character {
-
-  life: number;
-
-  lifePercent: number;
-
-  // Current mana or tech points (depends on the character class)
-  energy: number;
-
-  energyPercent: number;
-
-  // Character bonuses, a.k.a. "buffs"
-  bonuses: string[] = [];
-
-  // Character bonuses, a.k.a. "debuffs"
-  maluses: string[] = [];
-
-  constructor(
-    public name: string,
-    // Character class, could be an enum
-    public clazz: string,
-    public level: number,
-    public lifeMax: number,
-    public useMana: boolean,
-    // Max mana or tech points (depends on the character class)
-    public energyMax: number) {
-    this.life = lifeMax;
-    this.lifePercent = 100 * this.life / lifeMax;
-
-    this.energy = energyMax;
-    this.energyPercent = 100 * this.energy / energyMax;
   }
 }

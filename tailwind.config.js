81374202bccafbb50d0f7b7ebf7f64caf497998c
Florian Beaufumé <fbeaufume@gmail.com>
2022-01-29 10:55:30 +0100

    Added color aliasing to easily switch between sets of gray

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // gray: colors.slate,
+        // gray: colors.zinc,
+        gray: colors.neutral,
+        // gray: colors.stone,
+      }
+    },
   },
   plugins: [],
 }

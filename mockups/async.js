ce1792134b40631d1efbf0576d4f95e464d6e612
Florian Beaufumé <fbeaufume@gmail.com>
2023-04-01 11:32:19 +0200

    Improved asynchronous tests

--- mockups/async.js
+++ mockups/async.js
@@ -1,18 +1,18 @@
 // This small program shows that setTimeout and promise processing are both executed asynchronously,
 // but promise processing has a higher priority (they use microtasks) than setTimeout (that use macrotasks a.k.a. tasks).
-// This program prints the letters in order, i.e. A, then B, then C, etc.
+// This program prints the numbers in order, i.e. 1, then 2, then 3, etc.
 
-print('A');
+print(1);
 
-setTimeout(() => print('F'), 100);
+setTimeout(() => print(8), 100);
 
-setTimeout(() => print('E'), 0);
+setTimeout(() => print(7), 0);
 
-Promise.resolve('C').then(s => print(s));
+Promise.resolve().then(() => print(3)).then(() => print(5));
 
-Promise.resolve('D').then(s => print(s));
+Promise.resolve().then(() => print(4)).then(() => print(6));
 
-print('B');
+print(2);
 
 function print(message) {
   const date = new Date();

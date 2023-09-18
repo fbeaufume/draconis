36e0dd2ecdeb2900f9d70f67cf4115c4ca62f8d5
Florian Beaufumé <fbeaufume@gmail.com>
2023-09-18 18:42:26 +0200

    Second step of migration to standalone components (remove application module)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
 import {FightService} from './fight/fight.service';
+import { RouterOutlet } from '@angular/router';
+import { FightComponent } from './fight/fight.component';
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: true,
+    imports: [NgIf, FightComponent, RouterOutlet]
 })
 export class AppComponent {
 

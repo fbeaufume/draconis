2c7b2ba197e9d0975567260986e5f136c9bee771
Florian Beaufumé <fbeaufume@gmail.com>
2024-08-11 11:57:28 +0200

    Fixed or removed default Angular tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,6 +18,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, draconis');
+    expect(compiled.querySelector('p')?.textContent).toContain('Draconis');
   });
 });
